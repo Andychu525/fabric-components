f28b818090f122ec51f18bd2ed77fa8954bd736c
kunkun.chu@shopee.com <kunkun.chu@shopee.com>
2020-03-12 14:49:26 +0800

    del

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { initializeIcons } from 'office-ui-fabric-react'
-import * as serviceWorker from './serviceWorker'
-
-initializeIcons()
-
-ReactDOM.render(<div />, document.getElementById('root'))
-
-serviceWorker.unregister()
+export * from './Menu'
